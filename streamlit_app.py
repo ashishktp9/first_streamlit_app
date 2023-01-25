031eb8dd29e5092d8be96e43bcde3a5bca2e9f81
ashishktp9 <122263026+ashishktp9@users.noreply.github.com>
2023-01-25 16:57:04 +0530

    Update streamlit_app.py
    imported panda library
    read csv file
    output csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-
-streamlit.header('Burger Making')
-streamlit.title(' 🐔 Choose your base')
-streamlit.title('🥑🍞 choose tikka')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
